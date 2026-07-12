ea5a7a4838e1be316c637d60595e5e0712939076
fthomasvp <fellipecab@gmail.com>
2026-07-12 22:23:36 +0100

    refactor: add Biome.js to replace Prettier and ESLint

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  // See https://docs.astro.build/en/guides/deploy/github/
-  site: "https://fthomasvp.github.io",
-  server: {
-    headers: {
-      "Cache-Control": import.meta.env.PROD ? "max-age=31536000" : "no-cache",
-    },
-  },
-  vite: {
-    plugins: [tailwindcss()],
-  },
+	// See https://docs.astro.build/en/guides/deploy/github/
+	site: "https://fthomasvp.github.io",
+	server: {
+		headers: {
+			"Cache-Control": import.meta.env.PROD ? "max-age=31536000" : "no-cache",
+		},
+	},
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
